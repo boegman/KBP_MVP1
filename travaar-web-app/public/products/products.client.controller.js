e403663ebdb41fdc76ba6ad36eb120bd4789e148
tumbone <tumbone@gmail.com>
2016-12-15 16:48:57 +0200

    products catalogue loading successfully

--- travaar-web-app/public/products/products.client.controller.js
+++ travaar-web-app/public/products/products.client.controller.js
@@ -174,12 +174,106 @@ angular.module('products').
            console.log("Data added!");
        };
 
-      //  $scope.addHotel();
+       $scope.addHotel_2 = function() {
+          $scope.hotels.$add({
+            "address": "90 De Korte St, Johannesburg, 2000, South Africa",
+            "contact_number": "011 1841516",
+            "id": "hotel1",
+            "name": "Once in Joburg",
+            "products": [
+              {
+                "prodId": 1,
+                "shortDescription": "BADJA FITTED CAP",
+                "detailedDescription": "100% Cotton, Plastic peak, Fusing Hand Made in South Africa",
+                "image": [
+                  {
+                    "url": "./img/products/prod-1-1.jpg"
+                  },
+                  {
+                    "url": "./img/products/prod-1-2.jpg"
+                  }
+                ],
+                "costPrice": 200,
+                "displayPrice": 340,
+                "quantity": 10,
+                "origin": "Cape Town, South Africa",
+                "supplierRef": "FC_BADJA"
+              },
+              {
+                "prodId": 2,
+                "shortDescription": "Makulu Jumbo bag",
+                "detailedDescription": "Our largest bag makes for an ideal day bag.  The isiZulu word makulu means 'the biggest'.",
+                "image": [
+                  {
+                    "url": "./img/products/prod-2-1.jpg"
+                  },
+                  {
+                    "url": "./img/products/prod-2-2.jpg"
+                  }
+                ],
+                "costPrice": 450,
+                "displayPrice": 640,
+                "quantity": 10,
+                "origin": "Cape Town, South Africa",
+                "supplierRef": "MakuluJumbo"
+              },
+              {
+                "prodId": 3,
+                "shortDescription": "Doorstop",
+                "detailedDescription": "",
+                "image": [
+                  {
+                    "url": "./img/doorstop.jpg"
+                  }
+                ],
+                "costPrice": 200,
+                "displayPrice": 250,
+                "quantity": 10,
+                "origin": "South Africa",
+                "supplierRef": ""
+              },
+              {
+                "prodId": 4,
+                "shortDescription": "Hand Bag",
+                "detailedDescription": "",
+                "image": [
+                  {
+                    "url": "./img/handbag.jpg"
+                  }
+                ],
+                "costPrice": 800,
+                "displayPrice": 899,
+                "quantity": 10,
+                "origin": "South Africa",
+                "supplierRef": ""
+              },
+              {
+                "prodId": 5,
+                "shortDescription": "Colorful necklace",
+                "detailedDescription": "A colorful necklace made of beads",
+                "image": [
+                  {
+                    "url": "./img/beads-necklace-md.jpg"
+                  }
+                ],
+                "costPrice": 500,
+                "displayPrice": 585,
+                "quantity": 10,
+                "origin": "South Africa",
+                "supplierRef": ""
+              }
+            ]
+          });
+
+          console.log("Data added!");
+      };
+
+      //  $scope.addHotel_2();
 
       //Get hotel products
       $scope.hotel = {};
       $scope.getHotel = function(){
-        var hotel_index = '-KZ1Xi-GclC3Xy6nkYEs';
+        var hotel_index = '-KZ1tjKMBrimWj6k1pHb';
       var hotel_ref = firebase.database().ref().child("hotels/"+hotel_index);
         var hotelObj = $firebaseObject(hotel_ref);
         hotelObj.$loaded()
